73b295cf3c79571f40f9da03af8fe0851a322e85
moelwakil <m_emara24@yahoo.com>
2020-02-26 16:18:48 -0500

    Implement Generic Repository

---
+++ ShopApp/ShopApp.DataAccess.InMemory/InMemoryRepository.cs
@@ -0,0 +1,79 @@
+﻿using ShopApp.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.DataAccess.InMemory
+{
+    public class InMemoryRepository <T> where T : BaseEntity
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<T> items;
+        string className;
+
+        public InMemoryRepository()
+        {
+            className = typeof(T).Name;
+            items = cache[className] as List<T>;
+            if (items == null)
+            {
+                items = new List<T>();
+            }
+        }
+
+        public void Commit()
+        {
+            cache[className] = items;
+        }
+
+        public void Insert(T t)
+        {
+            items.Add(t);
+        }
+
+        public void Update(T t)
+        {
+            T tToUpdate = items.Find(i => i.Id == t.Id); 
+            if (tToUpdate == null)
+            {
+                throw new Exception(className + " Not Found");
+            } else
+            {
+                tToUpdate = t;
+            }
+        }
+
+        public T Find (string id)
+        {
+            T t = items.Find(i => i.Id == id);
+            if (t == null)
+            {
+                throw new Exception(className + " Not Found");
+            } else
+            {
+                return t;
+            }
+        }
+
+        public IQueryable<T> Collection()
+        {
+            return items.AsQueryable();
+        }
+
+        public void Delete (string id)
+        {
+            T t = items.Find(i => i.Id == id);
+            if (t == null)
+            {
+                throw new Exception(className + " Not Found");
+            }
+            else
+            {
+                items.Remove(t);
+            }
+        }
+    }
+}
