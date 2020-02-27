102f3934e4570b91e1963a40ee55cc9eb496c861
moelwakil <m_emara24@yahoo.com>
2020-02-27 11:00:01 -0500

    Implemented SQL Reporistory

---
+++ ShopApp/ShopApp.DataAccess.SQL/SQLRepository.cs
@@ -0,0 +1,56 @@
+﻿using ShopApp.Core.Contracts;
+using ShopApp.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.DataAccess.SQL
+{
+    public class SQLRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        internal DataContext context;
+        internal DbSet<T> dbSet;
+
+        public SQLRepository(DataContext _context)
+        {
+            context = _context;
+            dbSet = _context.Set<T>();
+        }
+        public IQueryable<T> Collection()
+        {
+            return dbSet;
+        }
+
+        public void Commit()
+        {
+            context.SaveChanges();
+        }
+
+        public void Delete(string id)
+        {
+            var t = Find(id);
+            if (context.Entry(t).State == EntityState.Detached)
+                dbSet.Attach(t);
+            dbSet.Remove(t);
+        }
+
+        public T Find(string id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public void Insert(T t)
+        {
+            dbSet.Add(t);
+        }
+
+        public void Update(T t)
+        {
+            dbSet.Attach(t);
+            context.Entry(t).State = EntityState.Modified;
+        }
+    }
+}
